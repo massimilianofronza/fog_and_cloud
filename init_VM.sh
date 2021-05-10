e67452ea5adab8ad50067dceaeb7cad4e2cdbc76
massimilianofronza <maxfronza97@gmail.com>
2021-05-10 09:41:41 +0200

    Get and create the ubuntu image

---
+++ init_VM.sh
@@ -0,0 +1,9 @@
+#!usr/bin/sh
+
+# Get an ubuntu image
+wget / -c https://cloud-images.ubuntu.com/bionic/current/bionic-server-cloudimg-amd64.img
+
+# Create the image
+openstack image create --disk-format qcow2 --container-format bare --public \
+--file /var/tmp/bionic-server-cloudimg-amd64.img \
+ubuntu-bionic-18.04
