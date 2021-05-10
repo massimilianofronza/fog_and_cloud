6281164f4c4ec30fff8619c4010bd8db57a55de3
massimilianofronza <maxfronza97@gmail.com>
2021-05-10 09:44:02 +0200

    Fix on init

--- init_VM.sh
+++ init_VM.sh
@@ -5,5 +5,5 @@ wget / -c https://cloud-images.ubuntu.com/bionic/current/bionic-server-cloudimg-
 
 # Create the image
 openstack image create --disk-format qcow2 --container-format bare --public \
---file /var/tmp/bionic-server-cloudimg-amd64.img \
+--file /bionic-server-cloudimg-amd64.img \
 ubuntu-bionic-18.04
