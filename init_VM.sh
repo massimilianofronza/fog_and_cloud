28bb30470ff21f06c063af989132db855015235f
massimilianofronza <maxfronza97@gmail.com>
2021-05-10 09:52:57 +0200

    Custom flavor

--- init_VM.sh
+++ init_VM.sh
@@ -7,3 +7,6 @@ wget / -c https://cloud-images.ubuntu.com/bionic/current/bionic-server-cloudimg-
 openstack image create --disk-format qcow2 --container-format bare --public \
 --file /bionic-server-cloudimg-amd64.img \
 ubuntu-bionic-18.04
+
+# Create a custom flavor
+openstack flavor create --ram 1024 --disk 5 --vcpus 1 --public mini.ubuntu
