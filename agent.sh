01eea7bbe5ffae6e5e67f186772d06db81eb4348
massimilianofronza <maxfronza97@gmail.com>
2021-05-12 16:54:52 +0200

    Fix on SSH agent script

--- agent.sh
+++ agent.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/sh
 
+SCRIPT="/bin/bash"
+
 # Agent configuration(to be launched on your local computer):
 ssh-agent
 ssh-add ~/.ssh/id_rsa
-ssh -A ${LOGIN}@${LAB_HOST}
+
+echo "Now cat this file and manually execute the following line"
+# ssh -A ${LOGIN}@${LAB_HOST}
