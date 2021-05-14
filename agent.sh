a35a4eaf2d13fde5c100e57a0593a08bcdaa3610
massimilianofronza <maxfronza97@gmail.com>
2021-05-14 10:07:59 +0200

    Cleaning

--- agent.sh
+++ agent.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/sh
 
-SCRIPT="/bin/bash"
+# Instructions to connect to the machines where there's the public key of
+# the admin under ~/.ssh/authorized_keys:
+# From your local computer, inside this repo:
+# > ./agent.sh
+# > ssh -A ${LOGIN}@${LAB_HOST}
+#
+# And you'll be in the VM. From there:
+# > ssh ubuntu@<floating IP>
+
 
 # Agent configuration(to be launched on your local computer):
 ssh-agent
