0f5b4f4ccb7eb5f6af8cd02928599b00db0d9553
massimilianofronza <maxfronza97@gmail.com>
2021-05-17 17:06:52 +0200

    Beautification of ansible scripts

--- _ansible/ansible.sh
+++ _ansible/ansible.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
 
-# To execute a verbose version and being able to tell which task to execute:
-ansible-playbook -v full_setup.yml --step
+echo "Want to build networks (y)es/(n)o: "
+read networks
+echo "Want to build security group/rules (y)es/(n)o: "
+read sec_group
+echo "Want to build instances (y)es/(n)o: "
+read servers
+
+if [ "$networks" = "Y" ] || [ "$networks" = "y" ]
+then
+    # To execute a verbose version and being able to tell which task to execute:
+    ansible-playbook -v network_setup.yml --step
+fi
+
+if [ "$sec_group" = "Y" ] || [ "$sec_group" = "y" ]
+then
+    ansible-playbook -v sec_group_setup.yml --step
+fi
+
+if [ "$servers" = "Y" ] || [ "$servers" = "y" ]
+then
+    ansible-playbook -v launch_instances.yml --step
+fi
 
--- _ansible/remove_ansible.sh
+++ _ansible/remove_ansible.sh
@@ -1,20 +1,87 @@
 #!/bin/bash
 
-# Remove everything added by ansible, in opposite order of creation
+# Remove everything added by ansible, in opposite order of possible creation
+
+echo "Want to delete instances (y)es/(n)o: "
+read servers
+echo "Want to delete security group/rules (y)es/(n)o: "
+read sec_group
+echo "Want to delete networks (y)es/(n)o: "
+read networks
 
 # First the instances
-openstack server delete ans_load_bal
-openstack server delete ans_web_server_1
-openstack server delete ans_web_server_2
-openstack server delete ans_database
+if [ "$servers" = "Y" ] || [ "$servers" = "y" ]
+then
+    servers="n"
 
-# Then security groups
-openstack security group delete ans_load_bal_sec_group
-openstack security group delete ans_web_server_sec_group
-openstack security group delete ans_database_sec_group
+    echo "Want to check available instances? (y)es/(n)o"
+    read servers
+    if [ "$servers" = "Y" ] || [ "$servers" = "y" ]
+    then
+        servers="n"
+
+        echo "Available servers(wait for it): "
+        openstack server list
+    fi
+
+    echo "Want to delete ans_load_bal, ans_web_server_* and ans_database? (y)es/(n)o"
+    read servers
+    if [ "$servers" = "Y" ] || [ "$servers" = "y" ]
+    then
+        openstack server delete ans_load_bal
+        openstack server delete ans_web_server_1
+        openstack server delete ans_web_server_2
+        openstack server delete ans_database
+    fi
+fi
 
 # Then the networks
-openstack network delete ans_net_first
-openstack network delete ans_net_second
-openstack network delete ans_net_third
+if [ "$networks" = "Y" ] || [ "$networks" = "y" ]
+then
+    networks="n"
+    
+    echo "Want to check available networks? (y)es/(n)o"
+    read networks
+    if [ "$networks" = "Y" ] || [ "$networks" = "y" ]
+    then
+        network="n"
+
+        echo "Available networks(wait for it): "
+        openstack network list
+    fi
+    
+    echo "Want to delete ans_net_first, ans_net_second and ans_net_third? (y)es/(n)o"
+    read networks
+    if [ "$networks" = "Y" ] || [ "$networks" = "y" ]
+    then
+        openstack network delete ans_net_first
+        openstack network delete ans_net_second
+        openstack network delete ans_net_third
+    fi
+fi
+
+# Then security groups
+if [ "$sec_group" = "Y" ] || [ "$sec_group" = "y" ]
+then
+    sec_group="n"
+
+    echo "Want to check available security groups? (y)es/(n)o"
+    read sec_group
+    if [ "$sec_group" = "Y" ] || [ "$sec_group" = "y" ]
+    then
+        sec_group="n"
+
+        echo "Available security groups(wait for it): "
+        openstack security group list
+    fi
+
+    echo "want to delete ans_load_bal_sec_group, ans_web_server_sec_group and ans_database_sec_group? (y)es/(n)o"
+    read sec_group    
+    if [ "$sec_group" = "Y" ] || [ "$sec_group" = "y" ]
+    then
+        openstack security group delete ans_load_bal_sec_group
+        openstack security group delete ans_web_server_sec_group
+        openstack security group delete ans_database_sec_group
+    fi
+fi
 
