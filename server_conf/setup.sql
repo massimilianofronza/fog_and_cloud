65ec962b47d23de30cfb28fdb11ce0a6f0a4e721
georgi3b <georgianabud@gmail.com>
2021-05-25 22:29:01 +0200

    Fixed some minor details, separated database userdata and changed users in setup.sql

--- server_conf/setup.sql
+++ server_conf/setup.sql
@@ -3,6 +3,9 @@ CREATE DATABASE ampache_database;
 CREATE USER 'database_user'@'localhost' IDENTIFIED WITH mysql_native_password BY 'pr0jectDATA=base';
 GRANT ALL PRIVILEGES ON ampache_database.* TO 'database_user'@'localhost';
 
-CREATE USER 'db_user'@'172.24.4.201' IDENTIFIED WITH mysql_native_password BY 'pr0jectDATA=base';
-GRANT ALL PRIVILEGES ON ampache_database.* TO 'db_user'@'172.24.4.201';
+CREATE USER 'database_user'@'172.24.4.0/255.255.255.0' IDENTIFIED WITH mysql_native_password BY 'pr0jectDATA=base';
+GRANT ALL PRIVILEGES ON ampache_database.* TO 'database_user'@'172.24.4.0/255.255.255.0';
+
+CREATE USER 'database_user'@'10.10.30.%' IDENTIFIED WITH mysql_native_password BY 'pr0jectDATA=base';
+GRANT ALL PRIVILEGES ON ampache_database.* TO 'database_user'@'10.10.30.%';
 
