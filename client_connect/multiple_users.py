a107e107a9ad36d16cebec219e836d01281917d0
georgi3b <georgianabud@gmail.com>
2021-06-05 21:13:10 +0200

    More users that listen and download songs

---
+++ client_connect/multiple_users.py
@@ -0,0 +1,81 @@
+import time
+import ampache
+import requests
+import random
+import json
+import os.path
+from os import path
+from xml.etree import ElementTree as ET
+
+# Server variables
+ampache_url = 'http://172.24.4.51/site'
+
+urls = [
+	'http://172.24.4.87',
+	'http://172.24.4.198'
+]
+
+# Users and their API keys
+users = {
+	'admin' : '97b38cb216d93087a3d8f966330ea094',
+	'georgi' : '191f108d69dacce4bd3b2fdcd9246d95',
+	'mass' : 'b0d5f4168caf5160a0cdecc7ce03b1e4',
+	'random' : 'c5c3ec286d4ef31243e4d780c1ff2bb2', 
+	'eval' : '10f2f5515527dc3398bf300a4b67563a',
+	'user0' : '', 
+	'user1' : '', 
+	'user2' : '', 
+	'user3' : '', 
+	'user4' : '', 
+	'user5' : '', 
+	'user6' : '', 
+	'user7' : '', 
+	'user8' : '', 
+	'user9' : ''
+}
+
+while True: 
+	time.sleep(random.randint(1,4)) # so that not all start at the same time
+	user, api_key = random.choice(list(users.items()))
+	#print(user); print(api_key)
+	encrypted_key = ampache.encrypt_string(api_key, user)
+	ampache_session = ampache.handshake(ampache_url, encrypted_key)
+	if ampache_session:
+		print(f"USER-{user}: Connection established!")
+	else: 
+		print(f"USER-{user}: Troubles in connecting to server!")
+
+	# Request the list of all songs
+	url = random.choice(list(urls))
+	songs_url = f'{ampache_url}/server/json.server.php?action=songs&auth={ampache_session}'
+	response = requests.get(songs_url)
+	content = response.json()
+	time.sleep(1)
+
+	# Get a random song 
+	n = len(content)
+	i = random.randint(0, n-1)
+	chosen = content[i]
+	song_id = chosen['id']
+	song_title = chosen['title']
+	print(f"USER-{user}: I am thinking of listening to {song_title}")
+	time.sleep(1)
+	
+	#these are the users for which we want to simulate high activity, but not save the songs
+	song_path = '/dev/null'
+
+	prob = random.randint(1,10)
+	if prob > 5:   # since we want more activity, give higher probability to listen
+ 		print(f"USER-{user}: I will listen to it!")
+		listen_to = ampache.stream(url, ampache_session, song_id, 'song', song_path)
+		time.sleep(5)
+	else: 
+		print(f"USER-{user}: Changed my mind...")
+		time.sleep(2)
+	
+	# Just to let the server know you are still there
+	my_ping = ampache.ping(ampache_url, ampache_session)
+	
+	gb = ampache.goodbye(ampache_url, ampache_session)
+	
+	time.sleep(random.randint(7,15))
